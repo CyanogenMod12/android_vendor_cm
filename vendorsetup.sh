ebdf2453b1f0836207cdb1de33ccbb771ad2d8d0
David Vaz Guijarro <vazdavid@icloud.com>
2015-02-21 21:27:34 +0100

    Up

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,6 @@
-for combo in $(curl -s https://raw.githubusercontent.com/CyanogenMod/hudson/master/cm-build-targets | sed -e 's/#.*$//' | grep cm-12.0 | awk {'print $1'})
+for combo in $(curl -s https://raw.githubusercontent.com/CyanogenMod12/manifest/lp5.0/build-targets | sed -e 's/#.*$//' | grep cm-12.0 | awk {'print $1'})
 do
     add_lunch_combo $combo
+    sh device/huawei/msm7x27a-common/patches/apply.sh
+    sh device/huawei/u8833/patches/apply.sh
 done
